8e7d2ec87f243fe34cda5ce191793f40d814cf4b
dkhelemendyk <dkhelemendyk@berkeley.edu>
2019-11-17 08:29:05 -0800

    initial commit: setup Firebase, add logo splash, add email authentication

---
+++ app/src/main/java/com/example/pockethandyman/LogoActivity.java
@@ -0,0 +1,29 @@
+package com.example.pockethandyman;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logo);
+
+        /*Duration of wait*/
+        int LOGO_DISPLAY_DURATION = 4000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the MainActivity. */
+                Intent mainIntent = new Intent(LogoActivity.this, EmailPasswordActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, LOGO_DISPLAY_DURATION);
+    }
+}
