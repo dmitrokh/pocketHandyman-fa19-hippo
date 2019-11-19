5253d0e12f2b5d353cc4bd5c371ce070b208947b
Connor Griffith <connor.griffith@berkeley.edu>
2019-11-18 16:24:34 -0800

    Working on Home screen.  I have the search bar and title.  Next will be tiles

---
+++ app/src/main/java/com/example/pockethandyman/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.pockethandyman;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+
+
+    }
+}
